3e0988d5e4007a791e2cd52fb7e74b068250f63f
bradt123 <bradtbrey1234@gmail.com>
2017-12-21 15:39:20 -0400

    atu

--- base/patch000001.sql
+++ base/patch000001.sql
@@ -6,3 +6,13 @@ CREATE TABLE shop.tproveedor (
 ) INHERITS (pxp.tbase)
 WITH (oids = false);
 /*****************************F-SCP-EGS-SHOP-1-21/12/2017*************/
+
+/*****************************I-SCP-BVP-SHOP-3-21/12/2017*************/
+CREATE TABLE shop.tusuario (
+  id_usuario SERIAL,
+  nombre VARCHAR(255),
+  apellido VARCHAR(255),
+  CONSTRAINT pk_tusuario_id_usuario PRIMARY KEY(id_usuario)
+) INHERITS (pxp.tbase)
+WITH (oids = false);
+/*****************************F-SCP-BVP-SHOP-3-21/12/2017*************/
